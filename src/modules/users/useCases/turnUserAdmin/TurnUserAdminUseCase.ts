aa971d1fdef728522d904cb59b62545ea3f89d68
Junior-Gaspar <junior.gaspar2@gmail.com>
2021-04-11 20:22:24 -0300

    :satisfied: challenge finalized

--- src/modules/users/useCases/turnUserAdmin/TurnUserAdminUseCase.ts
+++ src/modules/users/useCases/turnUserAdmin/TurnUserAdminUseCase.ts
@@ -3,14 +3,22 @@ import { IUsersRepository } from "../../repositories/IUsersRepository";
 
 interface IRequest {
   user_id: string;
-}
+};
 
 class TurnUserAdminUseCase {
   constructor(private usersRepository: IUsersRepository) {}
 
   execute({ user_id }: IRequest): User {
-    // Complete aqui
-  }
-}
+    const user = this.usersRepository.findById(user_id);
+
+    if (!user) {
+      throw new Error("User not found");
+    };
+
+    const userAdmin = this.usersRepository.turnAdmin(user);
+
+    return userAdmin;
+  };
+};
 
 export { TurnUserAdminUseCase };
